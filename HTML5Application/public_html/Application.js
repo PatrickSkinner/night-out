78b8b957fafb14c4163c459da7614867b2c6e192
cmowat <cmowat@oucs1535.otago.ac.nz>
2016-08-06 15:03:44 +1200

    Working splash screen on application load

--- HTML5Application/public_html/Application.js
+++ HTML5Application/public_html/Application.js
@@ -1,12 +1,20 @@
-function splashScreen(){
+splashScreen();
+
+function splashScreen() {
     splashImage = new Image();
-    splashImage.src = '';
-    splashImage.onload = function(){
-        drawImage(splashImage, 0, 0);
+    splashImage.src = 'Assets/Starting_Logo.png';
+    splashImage.onload = function () {
+        drawImage(0, 0, splashImage);
     };
     window.setTimeout(mainMenu, 2000);
 }
 
-function mainMenu(){
+function mainMenu() {
+    clear();
     
+    menuImage = new Image();
+    menuImage.src = '';
+    menuImage.onload = function () {
+        drawImage(0, 0, menuImage);
+    };
 }
