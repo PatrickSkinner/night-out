b47278b1eea479e07fbbb3b2d94773602b6ed067
pskinner <pskinner@oucs1536.otago.ac.nz>
2016-08-07 16:01:37 +1200

    Tidied test application.

--- HTML5Application/public_html/TestApp.js
+++ HTML5Application/public_html/TestApp.js
@@ -1,22 +1,9 @@
 //background
 drawRect(0,0,320,320,"#F2F2F2");
 
-//buttons, wrap this stuff in to an emulator function later
-
-//drawRect(90,65,150,40,"#0095a5");
-//drawRect(85,60,150,40,"#00bcd4");
-//drawText(115, 90, "Botany", "#FFFFFF");
-
+//buttons
 createButtonObject(85, 60, 150, 40, "#00bcd4", "#0095a5", "Botany", "#FFFFFF", mapTest, obj = {lat: -45.864518, long: 170.510971} );
-
-//drawRect(90,125,150,40,"#0095a5");
-//drawRect(85,120,150,40,"#00bcd4");
-//drawText(115, 150, "Owheo", "#FFFFFF");
 createButtonObject(85, 120, 150, 40, "#00bcd4", "#0095a5", "Owheo", "#FFFFFF", mapTest, obj = {lat: -45.866997, long: 170.518195});
-
-//drawRect(90,185,150,40,"#0095a5");
-//drawRect(85,180,150,40,"#00bcd4");
-//drawText(105, 210, "Octagon", "#FFFFFF");
 createButtonObject(85, 180, 150, 40, "#00bcd4", "#0095a5", "Octagon", "#FFFFFF", mapTest, obj = {lat: -45.874307, long: 170.504151});
 
 // Check if input is within button boundry, respond appropriately.
@@ -29,18 +16,4 @@ function checkInput(x, y){
             button.onClickFunction();
         }
     }
-//    if(60 < y && y < 105){
-//        clear();
-//        mapTest(-45.864518, 170.510971);   
-//    }
-//    
-//    if(120 < y && y < 165){
-//        clear();
-//        mapTest(-45.866997, 170.518195);   
-//    }
-//    
-//    if(180 < y && y < 225){
-//        clear();
-//        mapTest(-45.874307, 170.504151);   
-//    }
 }
