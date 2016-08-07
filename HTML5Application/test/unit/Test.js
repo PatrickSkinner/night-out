0274f81cd01b7357ac857e19655954d83a39a778
mlang <mlang@oucs1534.otago.ac.nz>
2016-08-07 14:37:58 +1200

    adding tests

--- HTML5Application/test/unit/Test.js
+++ HTML5Application/test/unit/Test.js
@@ -3,17 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 MyTestCase = TestCase("MyTestCase");
+MyTestCase.prototype.setup = function (){  
+   
+    
+    
+    
+};
 MyTestCase.prototype.testA = function(){
    assertEquals("works", testworks());
 };
 MyTestCase.prototype.testWithinBounds = function(){
-    assertTrue("didnt work with total size", withinBounds(100,100,90,90,10,10));
+    assertTrue(withinBounds(100,100,90,90,15,15));
     
     
-}
-
-
-
-
+};
+/*MyTestCase.prototype.testButtons = function(){
+    assertEquals("button x was wrong",10,createdButton.xLocation);
+    assertEquals("button y was wrong",10,createdButton.yLocation);
+    assertEquals("button width was wrong",10,createdButton.width);
+    assertEquals("button height was wrong",10,createdButton.height);
+    assertEquals("button colour was wrong","#00bcd4",createdButton.colour);
+    assertEquals("buttons shadowcolour was wrong","#0095a5",createdButton.shadowColour);
+    assertEquals("buttons text is wrong","button",createdButton.text);
+    assertEquals("buttons text colour is wrong","#FFFFFF",createdButton.textColour);
+    assertEquals("this should be null",null,createdButton.onClickFunction());
+    assertEquals("this should be null",null,createdButton.imagePath);
+    
+};
+*/
+MyTestCase.prototype.tearDown = function(){
+    createdButton = null;
+};
