7ee88b2e30dc3b286681b79457536069182e75f0
cmowat <cmowat@oucs1535.otago.ac.nz>
2016-08-07 16:26:41 +1200

    Created JSON file. Can load data in application

--- HTML5Application/public_html/Application.js
+++ HTML5Application/public_html/Application.js
@@ -1,4 +1,16 @@
-splashScreen();
+function loadData(name){
+    if(name === "pub"){
+       var data = getPubFile(); 
+    }else if(name === "club"){
+        var data = getClubFile();
+    } else if(name === "food"){
+        var data = getFoodFile();
+    } else {
+        var data = getTaxiFile();
+    }
+    return data;
+}
+
 
 function splashScreen() {
     splashImage = new Image();
@@ -18,8 +30,37 @@ function mainMenu() {
         drawImage(0, 0, menuImage);
     };
     
-    createButtonObject(20,20, 130, 130, "#00bcd4", "#0095a5", "Botany", "#FFFFFF", null, null );
-    createButtonObject(170,20, 130, 130, "#00bcd4", "#0095a5", "Botany", "#FFFFFF", null, null );
-    createButtonObject(20,170, 130, 130, "#00bcd4", "#0095a5", "Botany", "#FFFFFF", null, null );
-    createButtonObject(170,170, 130, 130, "#00bcd4", "#0095a5", "Botany", "#FFFFFF", null, null );
+    createButtonObject(20,20, 130, 130, "#00bcd4", "#0095a5", "Pubs", "#FFFFFF", toPub, loadData("pub") );
+    createButtonObject(170,20, 130, 130, "#00bcd4", "#0095a5", "Clubs", "#FFFFFF", toClub, loadData("club") );
+    createButtonObject(20,170, 130, 130, "#00bcd4", "#0095a5", "Food", "#FFFFFF", toFood, loadData("food") );
+    createButtonObject(170,170, 130, 130, "#00bcd4", "#0095a5", "Taxi", "#FFFFFF", toTaxi, loadData("taxi") );
+    loadData("JSONclubEmu.js");
 }
+
+
+
+function toPub(){
+    clearButtons();
+    clear();
+    console.log("toPub");
+}
+
+function toClub(){
+    clearButtons();
+    clear();
+    console.log("toClub");
+}
+
+function toFood(){
+    clearButtons();
+    clear();
+    console.log("toFood");
+}
+
+function toTaxi(){
+    clearButtons();
+    clear();
+    console.log("toTaxi");
+}
+
+splashScreen();
---
+++ HTML5Application/public_html/JSON.js
@@ -0,0 +1,269 @@
+
+var pub = {
+    "pubs": [
+        {
+            "name": "The Bog",
+            "latitude": 5642.3,
+            "longitude": 5352.2,
+            "address": "387 George St",
+            "city": "Dunedin",
+            "details": "Authentic Irish Pub. Get some Guinness in yer.",
+            "open": "Fri - Sun, 11:00 - 03:00",
+            "phone": "03 477 8035",
+            "logo": "theboglogo.png",
+            "photo": "thebogbarphoto.png",
+            "webLink": "http://thebog.co.nz/",
+            "rating": 4
+        },
+        {
+            "name": "Craft Bar",
+            "latitude": 3425.6,
+            "longitude": 7537.6,
+            "address": "10 The Octagon",
+            "city": "Dunedin",
+            "details": "Craft Bar turns 11 this year! Come down for $11 lunches and beer prices from 11 years ago. ",
+            "open": "Fri - Sat, 12:00 - 02:00",
+            "phone": "03 470 1426",
+            "logo": "craftbarlogo.png",
+            "photo": "craftbarbarphoto.png",
+            "webLink": "http://www.craftbar.co.nz",
+            "rating": 5
+        },
+        {
+            "name": "The Craic Irish Tavern",
+            "latitude": 3525.6,
+            "longitude": 5352.7,
+            "address": "24 The Octagon",
+            "city": "Dunedin",
+            "details": "Come on in tonight and enjoy the entertainment of our open mic night!",
+            "open": "24/7",
+            "phone": "03 479 0781",
+            "logo": "thecraiclogo.jpg",
+            "photo": "thecraicbarphoto.jpg",
+            "webLink": "http://thecraic.co.nz",
+            "rating": 3
+        },
+        {
+            "name": "Innocent Bystander",
+            "latitude": 3552.5,
+            "longitude": 6262.7,
+            "address": "Octagon Dunedin",
+            "city": "Dunedin",
+            "details": "Great Pizza and also you can vape hard there thanks to the outdoor dance area.",
+            "open": "Fri - Sun, 11:00 - 04:00",
+            "phone": "03 471 9222",
+            "logo": "IBlogo.jpg",
+            "photo": "IBbarphoto.jpg",
+            "webLink": "http://www.ratbagib.co.nz",
+            "rating": 4
+        },
+        {
+            "name": "Alibi",
+            "latitude": 3424.2,
+            "longitude": 9241.6,
+            "address": "1 Princes St",
+            "city": "Dunedin",
+            "details": "Come down and try our meal of the month!",
+            "open": "weekdays, 11:00 - 05:00",
+            "phone": "03 477 6740",
+            "logo": "alibilogo.png",
+            "photo": "alibibarphoto.png",
+            "webLink": "http://www.alibibar.co.nz",
+            "rating": 3
+        }
+]
+};
+
+
+function getPubFile() {
+    return pub;     
+}
+
+var club = {
+    "clubs": [
+        {
+            "name": "Boogie",
+            "latitude": 2341.0,
+            "longitude": 2312.2,
+            "address": "19 Frederick St",
+            "city": "Dunedin",
+            "details": "Boogie is a club that plays all the old songs everyone only likes when theyre drunk.",
+            "open": "Fri - Sun,  22:00 - 03:00",
+            "phone": "0225437542",
+            "logo": "boogielogo.png",
+            "photo": "boogieclubphoto.png",
+            "webLink": "http://www.facebook.com/boogie.nz",
+            "rating": 3
+        },
+        {
+            "name": "10 Bar",
+            "latitude": 1221.9,
+            "longitude": 8042.4,
+            "address": "10 The Octagon",
+            "city": "Dunedin",
+            "details": "Nightclub/21st/Function Venue/pirate/Entertainment!!",
+            "open": "Fri - Sun, 22:00 - 03:00",
+            "phone": "0275887447",
+            "logo": "10barlogo.png",
+            "photo": "10barclubphoto.png",
+            "weblink": "http://www.facebook.com/10bar.nz",
+            "rating": 5
+        },
+        {
+            "name": "Brimstone",
+            "latitude": 2425.2,
+            "longitude": 5321.3,
+            "address": "66 Princes St",
+            "city": "Dunedin",
+            "details": "This is the club for 30 year olds who realise they are going nowhere with their lives.",
+            "open": "Fri - Sun, 22:00 - 03:00",
+            "phone": "02102527570",
+            "logo": "brimstonelogo.jpg",
+            "photo": "brimstoneclubphoto.png",
+            "webLink": "http://www.facebook.com/brimstone-nightclub",
+            "rating": 2
+        },
+        {
+            "name": "The Break",
+            "latitude": 5532.5,
+            "longitude": 9234.4,
+            "address": "1 Great King St",
+            "city": "Dunedin",
+            "details": "A great place to hook up with desperate chicks after 2am.",
+            "open": "Sat 11:00 - 03:00",
+            "phone": "02102204134",
+            "logo": "thebreaklogo.jpg",
+            "photo": "thebreakclubphoto.jpg",
+            "webLink": "http://www.facebook.com/the-break",
+            "rating": 1
+        },
+        {
+            "name": "Suburbia",
+            "latitude": 3525.1,
+            "longitude": 6754.9,
+            "address": "153 Stuart St",
+            "city": "Dunedin",
+            "details": "This club is pretty sweet, the underground part is cool.",
+            "open": "Sat 10:00 - 03:00",
+            "phone": "02102527570",
+            "logo": "suburbialogo.png",
+            "photo": "suburbiaclubphoto.png",
+            "webLink": "http://www.facebook.com/suburbia.dunedin",
+            "rating": 4
+        }
+]
+};
+
+function getClubFile() {
+    return club;   
+}
+
+var food = {
+    "foodVenues": [
+        {
+            "name": "McDonalds",
+            "latitude": 4636.5,
+            "longitude": 5273.7,
+            "address": "232 George St",
+            "city": "Dunedin",
+            "category": "Burgers",
+            "details": "Yall know what Maccas is all about, fam.",
+            "open": "24/7",
+            "phone": "03 479 0576",
+            "logo": "mcdonaldslogo.jpg",
+            "photo": "mcdonaldsfoodphoto.jpg",
+            "webLink": "http://www.mcdonalds.co.nz",
+            "rating": 2
+        },
+        {
+            "name": "Dost Turkish Cafe",
+            "latitude": 3525.1,
+            "longitude": 2595.6,
+            "address": "15 Princes St",
+            "city": "Dunedin",
+            "category": "Kebabs",
+            "details": "Best Turkish kebabs in Dunedin!",
+            "open": "Mon - Sat, 10:00 - 02:00",
+            "phone": "03 477 2477",
+            "logo": "dostlogo.jpg",
+            "photo": "dostfoodphoto.jpg",
+            "webLink": "http://www.facebook.com/Dost-Turkish-Cafe",
+            "rating": 3
+        },
+        {
+            "name": "Velvet Burger",
+            "latitude": 8439.5,
+            "longitude": 5285.5,
+            "address": "375 George St",
+            "city": "Dunedin",
+            "category": "Burgers",
+            "details": "We think it should be a rule that no-one should ever cook on a Saturday night.",
+            "open": "24/7",
+            "phone": "03 473 5266",
+            "logo": "velvetburgerlogo.jpg",
+            "photo": "velvetburgerfoodphoto.jpg",
+            "webLink": "http://www.velvetburger.co.nz",
+            "rating": 5
+        },
+        {
+            "name": "Pita Pit",
+            "latitude": 3526.6,
+            "longitude": 6839.6,
+            "address": "371 George St",
+            "city": "Dunedin",
+            "category": "Wraps & Sandwiches",
+            "details": "The healthy choice!",
+            "open": "24/7",
+            "phone": "03 474 0774",
+            "logo": "pitapitlogo.jpeg",
+            "photo": "pitapitfoodphoto.jpg",
+            "webLink": "http://pitapit.co.nz/stores/dunedin",
+            "rating": 4
+        },
+        {
+            "name": "Night n Day",
+            "latitude": 6362.7,
+            "longitude": 9483.6,
+            "address": "18 Princes St",
+            "city": "Dunedin",
+            "category": "Convenience Store",
+            "details": "Get some hot chips or wedges with sour cream.",
+            "open": "24/7",
+            "phone": "03 477 2424",
+            "logo": "nightndaylogo.jpg",
+            "photo": "noghtndayfoodphoto.jpg",
+            "webLink": "http://www.nightnday.co.nz/stores/",
+            "rating": 2
+        }
+]
+};
+
+
+function getFoodFile(){     
+    return food;
+}
+
+var taxis = {
+    "taxis": [
+        {
+            "name": "Blue Bubble Taxis",
+            "number": "03 477 7777",
+            "webLink": "dunedin.bluebubbletaxi.co.nz"
+        },
+        {
+            "name": "City United Taxis",
+            "number": "03 455 5555",
+            "webLink": "http://www.cityunitedtaxis.co.nz"
+        },
+        {
+            "name": "Southern Taxis",
+            "number": "03 476 6300",
+            "webLink": "http://www.southerntaxis.co.nz"
+        }
+]
+};
+
+function getTaxiFile() {
+    return taxis;
+}
+
