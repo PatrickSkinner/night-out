d70b00475dd70a6143249f508b46ff0ce6087e42
pskinner <pskinner@oucs1533.otago.ac.nz>
2016-08-13 16:02:28 +1200

    Tidied application

--- HTML5Application/public_html/Application.js
+++ HTML5Application/public_html/Application.js
@@ -1,9 +1,6 @@
 var lastFunction;
 var lastParameter;
-
 var assets = {};
-
-var clickable = false;
 
 function loadData(name){
     if(name === "pub"){
@@ -46,48 +43,24 @@ function splashScreen() {
 }
 
 function mainMenu() {
-    clickable = false;
     clearButtons();
     clear();
     
     createButton(20, 20, 130, 130, "#00bcd4", "#0095a5", toNameList, obj = {list: loadData("pub"), function: venueDisplay} );
-    //imagePub = new Image();
-    //imagePub.src = 'Assets/Images/Buttons_Images/SQUARE_Pub.png';
-    //imagePub.onload = function () {
-    //    drawImage(20, 20, imagePub);
-    //};
+    drawImage(20, 20, assets["SQUARE_Pub"]);
     
     createButton(170, 20, 130, 130, "#00bcd4", "#0095a5", toNameList, obj = {list: loadData("club"), function: venueDisplay} );
-    //imageClub = new Image();
-    //imageClub.src = 'Assets/Images/Buttons_Images/SQUARE_Club.png';
-    //imageClub.onload = function () {
-    //    drawImage(170, 20, imageClub);
-    //};
-    
-    createButton(20, 170, 130, 130, "#00bcd4", "#0095a5", toNameList, obj = {list: loadData("food"), function: venueDisplay} );
-    //imageFood = new Image();
-    //imageFood.src = 'Assets/Images/Buttons_Images/SQUARE_Food.png';
-    //imageFood.onload = function () {
-    //    drawImage(20, 170, imageFood);
-    //};
-    
-    createButton(170, 170, 130, 130, "#00bcd4", "#0095a5", toNameList, obj = { list: loadData("taxi"), function: taxiDisplay} );
-//    imageTaxi = new Image();
-//    imageTaxi.src = 'Assets/Images/Buttons_Images/SQUARE_Taxi.png';
-//    imageTaxi.onload = function () {
-//        drawImage(170, 170, imageTaxi);
-//    };
-
-    drawImage(20, 20, assets["SQUARE_Pub"]);
     drawImage(170, 20, assets["SQUARE_Club"]);
+
+    createButton(20, 170, 130, 130, "#00bcd4", "#0095a5", toNameList, obj = {list: loadData("food"), function: venueDisplay} );
     drawImage(20, 170, assets["SQUARE_Food"]);
+
+    createButton(170, 170, 130, 130, "#00bcd4", "#0095a5", toNameList, obj = { list: loadData("taxi"), function: taxiDisplay} );
     drawImage(170, 170, assets["SQUARE_Taxi"]);
-    
-    clickable = true;
+
 }
 
 function toNameList(data){
-    clickable = false;
     clearButtons();
     clear();
     
@@ -104,12 +77,9 @@ function toNameList(data){
     
     createButton(20, 290, 280, 20, "#00bFd4", "#0095a5", goBack, null);
     drawText(135, 308, "Back", "#FFFFFF", 20);
-    
-    clickable = true;
 }
 
 function venueDisplay(data){
-    clickable = false;
     lastFunction = toNameList;
     lastParameter = data.list;
     
@@ -124,30 +94,25 @@ function venueDisplay(data){
     
     createButton(110, 290, 190, 20, "#00bFd4", "#0095a5", mapTest, obj = {lat: -45.864518, long: 170.510971});
     drawText(145, 308, "View on Map", "#FFFFFF", 20);
-    
-    clickable = true;
+
 }
 
 function taxiDisplay(data){
-    clickable = false;
     lastFunction = toNameList;
     lastParameter = data.list;
     
     clearButtons();
     clear();
-    
-    clickable = true;
+
 }
 
 // Check if input is within button boundry, respond appropriately.
 function checkInput(x, y){
-    if(clickable){
-        buttons = getButtons();
-        for (var key in buttons) {
-            button = buttons[key];
-            if (typeof button !== 'undefined' && withinBounds(x, y, button.xLocation, button.yLocation, button.width, button.height) ) {
-                button.onClickFunction();
-            }
+    buttons = getButtons();
+    for (var key in buttons) {
+        button = buttons[key];
+        if (typeof button !== 'undefined' && withinBounds(x, y, button.xLocation, button.yLocation, button.width, button.height) ) {
+            button.onClickFunction();
         }
     }
 }
